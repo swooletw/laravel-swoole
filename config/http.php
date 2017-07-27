acbde9d9debcc6e1da63155be7a00cd9f136994c
huangyi <huangyi@haochang.tv>
2017-07-27 12:31:45 +0800

    configurations

---
+++ config/http.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | HTTP server configurations.
+    |--------------------------------------------------------------------------
+    |
+    | @see https://wiki.swoole.com/wiki/page/274.html
+    |
+    */
+
+    'server' => [
+
+        'host' => env('HTTP_SERVER_HOST', '127.0.0.1'),
+
+        'port' => env('HTTP_SERVER_PORT', '1215'),
+
+        'options' => [
+
+            'pid_file' => env('HTTP_SERVER_OPTIONS_PID_FILE', storage_path('logs/http.pid')),
+
+            'log_file' => env('HTTP_SERVER_OPTIONS_LOG_FILE', storage_path('logs/http.log')),
+
+            'daemonize' => env('HTTP_SERVER_OPTIONS_DAEMONIZE', 1),
+
+        ],
+
+    ],
+
+];
