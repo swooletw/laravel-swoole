9a3490338a11da58ce82e42ad844e296c9c5992f
huangyi <huangyi@haochang.tv>
2017-07-27 12:32:42 +0800

    HttpServiceProvider

---
+++ src/HttpServiceProvider.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace HuangYi\Http;
+
+use Illuminate\Support\ServiceProvider;
+
+class HttpServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
+
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+
+    }
+}
