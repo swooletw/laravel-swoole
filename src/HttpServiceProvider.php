4771b5c5c8667d8fb9eeb5543e915a795e7febca
huangyi <huangyi@haochang.tv>
2017-07-27 12:39:12 +0800

    Add copyright

--- src/HttpServiceProvider.php
+++ src/HttpServiceProvider.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the huang-yi/laravel-swoole-http package.
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 namespace HuangYi\Http;
 
 use Illuminate\Support\ServiceProvider;
