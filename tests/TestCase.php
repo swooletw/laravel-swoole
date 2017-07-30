0a15b6f7b66d18595cc98de776dbc527a5a91ecd
huangyi <coodeer@163.com>
2017-07-31 07:48:31 +0800

    TestCase

---
+++ tests/TestCase.php
@@ -0,0 +1,19 @@
+<?php
+
+/*
+ * This file is part of the huang-yi/laravel-swoole-http package.
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\Http\Tests;
+
+use Orchestra\Testbench\TestCase as BaseTestCase;
+
+class TestCase extends BaseTestCase
+{
+
+}
