f053c1339ce61edb94d0626d47fc1fef95235dd6
huangyi <coodeer@163.com>
2017-07-31 07:49:27 +0800

    Response tests

---
+++ tests/Server/ResponseTest.php
@@ -0,0 +1,22 @@
+<?php
+
+/*
+ * This file is part of the huang-yi/laravel-swoole-http package.
+ *
+ * (c) Huang Yi <coodeer@163.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace HuangYi\Http\Tests\Server;
+
+use HuangYi\Http\Tests\TestCase;
+
+class ResponseTest extends TestCase
+{
+    public function testMake()
+    {
+
+    }
+}
