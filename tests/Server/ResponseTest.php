338cd84846bf824e3fd311be5d1dfe417855ec63
huangyi <huangyi@haochang.tv>
2017-07-31 11:23:54 +0800

    test Response

--- tests/Server/ResponseTest.php
+++ tests/Server/ResponseTest.php
@@ -11,12 +11,33 @@
 
 namespace HuangYi\Http\Tests\Server;
 
+use HuangYi\Http\Server\Response;
 use HuangYi\Http\Tests\TestCase;
+use Illuminate\Http\Response as IlluminateResponse;
+use Swoole\Http\Response as SwooleResponse;
 
 class ResponseTest extends TestCase
 {
     public function testMake()
     {
+        $response = Response::make(new IlluminateResponse, new SwooleResponse);
 
+        $this->assertInstanceOf(Response::class, $response);
+    }
+
+    public function testGetIlluminateResponse()
+    {
+        $response = Response::make(new IlluminateResponse, new SwooleResponse);
+        $illuminateResponse = $response->getIlluminateResponse();
+
+        $this->assertInstanceOf(IlluminateResponse::class, $illuminateResponse);
+    }
+
+    public function testGetSwooleResponse()
+    {
+        $response = Response::make(new IlluminateResponse, new SwooleResponse);
+        $swooleResponse = $response->getSwooleResponse();
+
+        $this->assertInstanceOf(SwooleResponse::class, $swooleResponse);
     }
 }
