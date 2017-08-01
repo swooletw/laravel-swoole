98b72858d70fed3326383af00a98d0eb9f9de38d
huangyi <huangyi@haochang.tv>
2017-08-01 19:00:37 +0800

    fix log file suffix

--- config/http.php
+++ config/http.php
@@ -21,7 +21,7 @@ return [
 
             'pid_file' => env('HTTP_SERVER_OPTIONS_PID_FILE', base_path('storage/logs/http.pid')),
 
-            'log_file' => env('HTTP_SERVER_OPTIONS_LOG_FILE', base_path('storage/logs/http.logs')),
+            'log_file' => env('HTTP_SERVER_OPTIONS_LOG_FILE', base_path('storage/logs/http.log')),
 
             'daemonize' => env('HTTP_SERVER_OPTIONS_DAEMONIZE', 1),
 
