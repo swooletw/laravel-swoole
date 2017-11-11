5ba2ebdd224cfccaa8293a2c965c6f630c23bebb
Albert Chen <albert.cht@gmail.com>
2017-11-11 15:41:56 +0800

    set daemonize to be false in default

--- config/swoole_http.php
+++ config/swoole_http.php
@@ -15,7 +15,7 @@ return [
         'options' => [
             'pid_file' => env('SWOOLE_HTTP_PID_FILE', base_path('storage/logs/swoole_http.pid')),
             'log_file' => env('SWOOLE_HTTP_LOG_FILE', base_path('storage/logs/swoole_http.log')),
-            'daemonize' => env('SWOOLE_HTTP_DAEMONIZE', 1),
+            'daemonize' => env('SWOOLE_HTTP_DAEMONIZE', 0),
         ],
     ],
 ];
