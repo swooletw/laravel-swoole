450c12f275f5aa1a0f151b6d1f4f7b1b936fffe7
Albert Chen <albert.cht@gmail.com>
2018-04-18 17:50:14 +0800

    set alias for swoole.server

--- src/Server/Manager.php
+++ src/Server/Manager.php
@@ -324,6 +324,8 @@ class Manager
         $this->app->singleton('swoole.server', function () {
             return $this->server;
         });
+        $this->app->alias(HttpServer::class, 'swoole.server');
+        $this->app->alias(WebSocketServer::class, 'swoole.server');
     }
 
     /**
