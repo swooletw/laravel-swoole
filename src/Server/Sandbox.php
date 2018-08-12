5703a55442f7105b5d35996075624831fdca516e
to2false <to2false@gmail.com>
2018-08-12 23:05:02 +0800

    Flush snapshot to clean up

--- src/Server/Sandbox.php
+++ src/Server/Sandbox.php
@@ -310,6 +310,7 @@ class Sandbox
         }
 
         if ($this->snapshot instanceOf Application) {
+            $this->snapshot->getApplication()->flush();
             $this->snapshot = null;
         }
 
