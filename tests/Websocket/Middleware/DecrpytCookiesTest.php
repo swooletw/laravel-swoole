9c6cc28e38f543b96a3834da1131298ba42631c7
Albert Chen <albert.cht@gmail.com>
2018-08-28 23:38:37 +0800

    add tests for DecryptCookies middleware

---
+++ tests/Websocket/Middleware/DecrpytCookiesTest.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace SwooleTW\Http\Tests\Websocket\Middleware;
+
+use Mockery as m;
+use Illuminate\Http\Request;
+use SwooleTW\Http\Tests\TestCase;
+use Symfony\Component\HttpFoundation\ParameterBag;
+use SwooleTW\Http\Websocket\Middleware\DecryptCookies;
+use Illuminate\Contracts\Encryption\Encrypter as EncrypterContract;
+
+class DecrpytCookiesTest extends TestCase
+{
+    public function testDisableFor()
+    {
+        $middleware = $this->getMiddleware();
+        $middleware->disableFor('foo');
+
+        $this->assertTrue($middleware->isDisabled('foo'));
+        $this->assertFalse($middleware->isDisabled('bar'));
+    }
+
+    public function testDecrypt()
+    {
+        $request = new Request;
+        $request->cookies = new ParameterBag([
+            'foo' => 'bar',
+            'seafood' => 'sasaya',
+            'aaa' => [
+                'bbb' => 'ccc'
+            ]
+        ]);
+
+        $encrypter = m::mock(EncrypterContract::class);
+        $encrypter->shouldReceive('decrypt')
+            ->once()
+            ->with('sasaya', false);
+        $encrypter->shouldReceive('decrypt')
+            ->once()
+            ->with('ccc', false);
+
+        $middleware = $this->getMiddleware($encrypter);
+        $middleware->disableFor('foo');
+
+        $middleware->handle($request, function ($next) {
+            return $next;
+        });
+    }
+
+    protected function getMiddleware(EncrypterContract $encrypter = null)
+    {
+        return new DecryptCookies($encrypter ?: m::mock(EncrypterContract::class));
+    }
+}
