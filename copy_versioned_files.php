fa965b77e2f87eedddaf25740d7ac12e04983d3a
Albert Chen <albert.cht@gmail.com>
2019-03-07 09:56:35 +0800

    make compatible for dotenv 3

--- copy_versioned_files.php
+++ copy_versioned_files.php
@@ -2,12 +2,18 @@
 
 require __DIR__.'/vendor/autoload.php';
 
-use SwooleTW\Http\Coroutine\Connectors\ConnectorFactory;
 use SwooleTW\Http\Helpers\FW;
 use SwooleTW\Http\Task\QueueFactory;
+use SwooleTW\Http\Coroutine\Connectors\ConnectorFactory;
 
-$version = FW::version();
-$framework = ucfirst(FW::current());
+try {
+    $framework = ucfirst(FW::current());
+    $version = FW::version();
+} catch (Throwable $e) {
+    echo "No files were generated.\n";
+    die;
+}
+
 $color = "\033[0;32m";
 $noColor = "\033[0m";
 $stubs = [];
