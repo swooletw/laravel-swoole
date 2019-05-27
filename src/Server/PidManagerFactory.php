f1adf651adbb4d13160b3de91bbb62928fb0888e
Bill li <bill.li@ofcold.com>
2019-05-27 12:10:42 +0800

    remove Psr\Container\ContainerInterface

--- src/Server/PidManagerFactory.php
+++ src/Server/PidManagerFactory.php
@@ -2,16 +2,12 @@
 
 namespace SwooleTW\Http\Server;
 
-use Psr\Container\ContainerInterface;
-
 class PidManagerFactory
 {
-	public function __invoke(ContainerInterface $container) : PidManager
+	public function __invoke() : PidManager
 	{
-		$config = $container->get('config');
-
 		return new PidManager(
-			$config->get('swoole_http.server.options.pid_file') ?? sys_get_temp_dir() . '/swoole.pid'
+			config('swoole_http.server.options.pid_file') ?? sys_get_temp_dir() . '/swoole.pid'
 		);
 	}
 }
--- tests/Server/PidManagerFactoryTest.php
+++ tests/Server/PidManagerFactoryTest.php
@@ -2,7 +2,6 @@
 
 namespace SwooleTW\Http\Tests\Server;
 
-use Psr\Container\ContainerInterface;
 use SwooleTW\Http\Server\PidManager;
 use SwooleTW\Http\Server\PidManagerFactory;
 use SwooleTW\Http\Tests\TestCase;
@@ -13,8 +12,6 @@ class PidManagerFactoryTest extends TestCase
     {
         $factory = new PidManagerFactory();
 
-        $pidManager = $factory($this->prophesize(ContainerInterface::class)->reveal());
-
-        $this->assertInstanceOf(PidManager::class, $pidManager);
+        $this->assertInstanceOf(PidManager::class, $factory());
     }
 }
