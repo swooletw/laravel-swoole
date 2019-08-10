0351741bcf276f6f64bb20bd13e96692cff62797
Yuriy Potemkin <ysoft2000@mail.ru>
2019-08-10 13:35:36 +0300

    fixes for working with websocket in jobs

--- src/Websocket/Websocket.php
+++ src/Websocket/Websocket.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\App;
 use Illuminate\Support\Facades\Config;
 use InvalidArgumentException;
 use SwooleTW\Http\Server\Facades\Server;
+use SwooleTW\Http\Server\Manager;
 use SwooleTW\Http\Websocket\Rooms\RoomContract;
 
 /**
@@ -172,17 +173,33 @@ class Websocket
             return false;
         }
 
-        $result = App::make(Server::class)->task([
-            'action' => static::PUSH_ACTION,
-            'data' => [
-                'sender' => $this->sender,
-                'fds' => $fds,
+        /** @var Server $server */
+        $server = App::make(Server::class);
+
+        if ($server->taskworker) {
+            /** @var Manager $manager */
+            $manager = App::make(Manager::class);
+            $result = $manager->pushMessage($server, [
+                'sender'    => $this->sender,
+                'fds'       => $fds,
                 'broadcast' => $this->isBroadcast,
-                'assigned' => $assigned,
-                'event' => $event,
-                'message' => $data,
-            ],
-        ]);
+                'assigned'  => $assigned,
+                'event'     => $event,
+                'message'   => $data,
+            ]);
+        } else {
+            $result = $server->task([
+                'action' => static::PUSH_ACTION,
+                'data'   => [
+                    'sender'    => $this->sender,
+                    'fds'       => $fds,
+                    'broadcast' => $this->isBroadcast,
+                    'assigned'  => $assigned,
+                    'event'     => $event,
+                    'message'   => $data,
+                ],
+            ]);
+        }
 
         $this->reset();
 
