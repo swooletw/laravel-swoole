cd50a39b2896f46924b5479aedbb6954e1d12fa3
Zachary He <zachary.h@wbstech.com.au>
2020-12-04 18:57:49 +1100

    Add Gzip support on chunk

--- tests/Transformers/ResponseTest.php
+++ tests/Transformers/ResponseTest.php
@@ -4,6 +4,7 @@ namespace SwooleTW\Http\Tests\Transformers;
 
 use Illuminate\Http\Response as IlluminateResponse;
 use Swoole\Http\Response as SwooleResponse;
+use Swoole\Http\Request as SwooleRequest;
 use SwooleTW\Http\Tests\TestCase;
 use SwooleTW\Http\Transformers\Response;
 
@@ -11,14 +12,14 @@ class ResponseTest extends TestCase
 {
     public function testMake()
     {
-        $response = Response::make(new IlluminateResponse, new SwooleResponse);
+        $response = Response::make(new IlluminateResponse, new SwooleResponse, new SwooleRequest);
 
         $this->assertInstanceOf(Response::class, $response);
     }
 
     public function testGetIlluminateResponse()
     {
-        $response = Response::make(new IlluminateResponse, new SwooleResponse);
+        $response = Response::make(new IlluminateResponse, new SwooleResponse, new SwooleRequest);
         $illuminateResponse = $response->getIlluminateResponse();
 
         $this->assertInstanceOf(IlluminateResponse::class, $illuminateResponse);
@@ -26,7 +27,7 @@ class ResponseTest extends TestCase
 
     public function testGetSwooleResponse()
     {
-        $response = Response::make(new IlluminateResponse, new SwooleResponse);
+        $response = Response::make(new IlluminateResponse, new SwooleResponse, new SwooleRequest);
         $swooleResponse = $response->getSwooleResponse();
 
         $this->assertInstanceOf(SwooleResponse::class, $swooleResponse);
